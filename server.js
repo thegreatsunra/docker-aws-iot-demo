05eef540a06eb99204acaa4f4e56292e0cb1d833
Dane Petersen <thegreatsunra@gmail.com>
2017-06-01 16:18:04 -0500

    Add AWS IoT scripts, deps, and ignores

--- server.js
+++ server.js
@@ -1,15 +1,110 @@
-'use strict'
+/*
+ * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
-const express = require('express')
+// node.js deps
 
-// Constants
-const PORT = 8080
+// npm deps
 
-// App
-const app = express()
-app.get('/', function (req, res) {
-  res.send('Hello world\n')
-})
+// app deps
+const deviceModule = require('aws-iot-device-sdk').device
+const cmdLineProcess = require('aws-iot-device-sdk/examples/lib/cmdline')
 
-app.listen(PORT)
-console.log('Running on http://localhost:' + PORT)
+// begin module
+
+function processTest (args) {
+  //
+  // The device module exports an MQTT instance, which will attempt
+  // to connect to the AWS IoT endpoint configured in the arguments.
+  // Once connected, it will emit events which our application can
+  // handle.
+  //
+  const device = deviceModule({
+    keyPath: args.privateKey,
+    certPath: args.clientCert,
+    caPath: args.caCert,
+    clientId: args.clientId,
+    region: args.region,
+    baseReconnectTimeMs: args.baseReconnectTimeMs,
+    keepalive: args.keepAlive,
+    protocol: args.Protocol,
+    port: args.Port,
+    host: args.Host,
+    debug: args.Debug
+  })
+
+  let timeout // eslint-disable-line no-unused-vars
+  let count = 0
+  const minimumDelay = 250
+
+  if (args.testMode === 1) {
+    device.subscribe('topic_1')
+  } else {
+    device.subscribe('topic_2')
+  }
+  if ((Math.max(args.delay, minimumDelay)) !== args.delay) {
+    console.log('substituting ' + minimumDelay + 'ms delay for ' + args.delay + 'ms...')
+  }
+  timeout = setInterval(() => {
+    count++
+
+    if (args.testMode === 1) {
+      device.publish('topic_2', JSON.stringify({
+        mode1Process: count
+      }))
+    } else {
+      device.publish('topic_1', JSON.stringify({
+        mode2Process: count
+      }))
+    }
+  }, Math.max(args.delay, minimumDelay)) // clip to minimum
+
+  //
+  // Do a simple publish/subscribe demo based on the test-mode passed
+  // in the command line arguments.  If test-mode is 1, subscribe to
+  // 'topic_1' and publish to 'topic_2' otherwise vice versa.  Publish
+  // a message every four seconds.
+  //
+  device
+    .on('connect', () => {
+      console.log('connect')
+    })
+  device
+    .on('close', () => {
+      console.log('close')
+    })
+  device
+    .on('reconnect', () => {
+      console.log('reconnect')
+    })
+  device
+    .on('offline', () => {
+      console.log('offline')
+    })
+  device
+    .on('error', (error) => {
+      console.log('error', error)
+    })
+  device
+    .on('message', (topic, payload) => {
+      console.log('message', topic, payload.toString())
+    })
+}
+
+module.exports = cmdLineProcess
+
+if (require.main === module) {
+  cmdLineProcess('connect to the AWS IoT service and publish/subscribe to topics using MQTT, test modes 1-2',
+  process.argv.slice(2), processTest)
+}
